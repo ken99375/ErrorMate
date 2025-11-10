f6123556e05a478b9ea201e2e488af1abe0cb071
ken99375 <ktq2490316@stu.o-hara.ac.jp>
2025-11-10 10:05:10 +0900

    app.py変更

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from blueprints.main import main_bp
 
-# Flaskアプリのインスタンスを作成
+# 他のBlueprintはファイル作成後にコメントアウトを解除する
+# from blueprints.auth import auth_bp
+# from blueprints.step_card import step_card_bp
+# from blueprints.share import share_bp
+# from blueprints.admin import admin_bp
+# from blueprints.api import api_bp
+
 app = Flask(__name__)
+app.secret_key = 'your_secret_key' # 開発用キー
 
-# http://127.0.0.1:5000/ にアクセスしたときの処理
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+# main_bpのみ登録
+app.register_blueprint(main_bp)
+
+# 将来的に有効化する
+# app.register_blueprint(auth_bp, url_prefix='/auth')
+# app.register_blueprint(step_card_bp, url_prefix='/card')
+# app.register_blueprint(share_bp, url_prefix='/library')
+# app.register_blueprint(admin_bp, url_prefix='/admin')
+# app.register_blueprint(api_bp, url_prefix='/api')
+
+if __name__ == '__main__':
+    app.run(debug=True)
